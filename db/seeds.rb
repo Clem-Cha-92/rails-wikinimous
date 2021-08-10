087da3440e57dedf02bd069a7f6a337b271ed32f
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-10 16:39:52 +0200

    Completed Rails Wikinimous structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# generate 10 articles
+10.times do
+  article = Article.new(
+    title: Faker::Company.bs,
+    content: Faker::Movies::BackToTheFuture.quote
+  )
+  article.save!
+end
